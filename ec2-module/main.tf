b43385123cfa06a822cfda0b3d62cfff6673e7f8
Dilshan Rodrigo <dilshanrodrigo@Dilshans-MacBook-Pro.local>
2025-03-31 16:48:08 +1100

    EC2 Update

--- ec2-module/main.tf
+++ ec2-module/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2_module_test" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
-    instance_name = var.Instance_Name
+  
   
 
     tags = {
