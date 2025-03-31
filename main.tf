fc23e84b2bbd73de110f3384e87da51c3b1c78e9
Dilshan Rodrigo <dilshanrodrigo@Dilshans-MacBook-Pro.local>
2025-03-31 16:21:40 +1100

    update EC2 Module

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
-# ec2 module
-resource "aws_instance" "ec2_module_test" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    key_name = var.key_name
-
-    tags = {
-        name = var.Instance_Name
-    }
+provider "aws"{
+    region = "ap-southeast-2"
 }
+
+resource "aws_s3_bucket" "tftest-bucket" {
+    bucket = "tf-test-bucket-dilshanr"
+    acl = "private"
+
+}
+
+# Use ec2-module #
+module "ec2_module"{
+    source = "./ec2-module"
+    ami_id  = "ami-034488765f896f58f"
+    key_name = "tfkeypair"
+    instance_name = "TestTFInstance"
+ }
